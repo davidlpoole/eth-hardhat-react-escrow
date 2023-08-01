c1b8390fe74c6e0bb1f66da1382c6fd60524b70a
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-08-01 13:00:32 +0100

    Function to submit and approve based on contract addr

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,8 @@ import { ethers } from 'ethers';
 import { useEffect, useState } from 'react';
 import deploy from './deploy';
 import Escrow from './Escrow';
+import EscrowContract from './artifacts/contracts/Escrow.sol/Escrow';
+
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 
@@ -10,14 +12,30 @@ export async function approve(escrowContract, signer) {
   await approveTxn.wait();
 }
 
+export async function approveByAddress(escrowAddress, signer) {
+  console.log(escrowAddress);
+  console.log(EscrowContract.abi);
+  console.log(signer);
+  const contract = new ethers.Contract(escrowAddress, EscrowContract.abi, signer);
+  console.log(contract);
+  const approveTxn = await contract.connect(signer).approve();
+  console.log(approveTxn);
+  const response = await approveTxn.wait();
+  console.log(response);
+
+}
+
 function App() {
   const [escrows, setEscrows] = useState([]);
+  const [escrowAddresses, setEscrowAddresses] = useState([]);
   const [account, setAccount] = useState();
   const [signer, setSigner] = useState();
+  const [accounts, setAccounts] = useState([]);
 
   useEffect(() => {
     async function getAccounts() {
       const accounts = await provider.send('eth_requestAccounts', []);
+      setAccounts(accounts);
 
       setAccount(accounts[0]);
       setSigner(provider.getSigner());
@@ -26,15 +44,16 @@ function App() {
     getAccounts();
   }, [account]);
 
+
   async function newContract() {
     const beneficiary = document.getElementById('beneficiary').value;
     const arbiter = document.getElementById('arbiter').value;
     const value = ethers.BigNumber.from(document.getElementById('wei').value);
     const escrowContract = await deploy(signer, arbiter, beneficiary, value);
 
-
     const escrow = {
       address: escrowContract.address,
+      sender: account,
       arbiter,
       beneficiary,
       value: value.toString(),
@@ -45,16 +64,21 @@ function App() {
           document.getElementById(escrowContract.address).innerText =
             "✓ It's been approved!";
         });
-
         await approve(escrowContract, signer);
       },
     };
 
     setEscrows([...escrows, escrow]);
+    setEscrowAddresses([...escrowAddresses, escrow.address])
   }
 
   return (
-    <>
+    <><div>Debug:</div>
+      <div>Current account: {account}</div>
+      <div>All accounts: {JSON.stringify(accounts)}</div>
+      <div>All contracts: {JSON.stringify(escrows)}</div>
+      <div>All contracts: {JSON.stringify(escrowAddresses)}</div>
+
       <div className="contract">
         <h1> New Contract </h1>
         <label>
@@ -77,7 +101,6 @@ function App() {
           id="deploy"
           onClick={(e) => {
             e.preventDefault();
-
             newContract();
           }}
         >
@@ -92,6 +115,24 @@ function App() {
           {escrows.map((escrow) => {
             return <Escrow key={escrow.address} {...escrow} />;
           })}
+        </div>
+      </div>
+
+      <div className="existing-contracts">
+        <h1> Approve </h1>
+
+        <div id="container">
+          <label>
+            Contract Address
+            <input type="text" id="contractAddr" />
+          </label>
+          <div
+              className="button"
+              id='approve'
+              onClick={()=>{approveByAddress(document.getElementById('contractAddr').value, signer)}}
+          >
+            Approve
+          </div>
         </div>
       </div>
     </>
--- app/src/Escrow.js
+++ app/src/Escrow.js
@@ -1,37 +1,45 @@
 export default function Escrow({
-  address,
-  arbiter,
-  beneficiary,
-  value,
-  handleApprove,
-}) {
-  return (
-    <div className="existing-contract">
-      <ul className="fields">
-        <li>
-          <div> Arbiter </div>
-          <div> {arbiter} </div>
-        </li>
-        <li>
-          <div> Beneficiary </div>
-          <div> {beneficiary} </div>
-        </li>
-        <li>
-          <div> Value </div>
-          <div> {value} </div>
-        </li>
-        <div
-          className="button"
-          id={address}
-          onClick={(e) => {
-            e.preventDefault();
-
-            handleApprove();
-          }}
-        >
-          Approve
+                                   address,
+                                   sender,
+                                   arbiter,
+                                   beneficiary,
+                                   value,
+                                   handleApprove,
+                               }) {
+    return (
+        <div className="existing-contract">
+            <ul className="fields">
+                <li>
+                    <div> Contract</div>
+                    <div> {address} </div>
+                </li>
+                <li>
+                    <div> Sender</div>
+                    <div> {sender} </div>
+                </li>
+                <li>
+                    <div> Arbiter</div>
+                    <div> {arbiter} </div>
+                </li>
+                <li>
+                    <div> Beneficiary</div>
+                    <div> {beneficiary} </div>
+                </li>
+                <li>
+                    <div> Value</div>
+                    <div> {value} </div>
+                </li>
+                <div
+                    className="button"
+                    id={address}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleApprove();
+                    }}
+                >
+                    Approve
+                </div>
+            </ul>
         </div>
-      </ul>
-    </div>
-  );
+    );
 }
